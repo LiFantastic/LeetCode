581e8313b12166758bd97266b9a8303e3c0e5ef3
LiFantastic <fanli.cs.ucla@gmail.com>
2015-10-07 10:53:41 -0700

    code update

---
+++ #004.cpp
@@ -0,0 +1,34 @@
+/*============================================================
+Problem: Median of Two Sorted Arrays
+There are two sorted arrays nums1 and nums2 of size m and n 
+respectively. Find the median of the two sorted arrays. The 
+overall run time complexity should be O(log (m+n)).
+============================================================*/
+class Solution {
+private:
+    double findKth(vector<int>& A, int startA, int lenA, vector<int>& B, int startB, int lenB, int k) {
+        if (lenA > lenB) // make the first array not longer than the second one
+            return findKth(B, startB, lenB, A, startA, lenA, k);
+        if (lenA == 0)
+            return B[startB+k-1];
+        if (k == 1)
+            return min(A[startA], B[startB]);
+            
+        int partA = min(k/2, lenA), partB = k-partA;
+        
+        if (A[startA+partA-1] < B[startB+partB-1]) // the partA is all range in the smaller k^th elements
+            return findKth(A, startA+partA, lenA-partA, B, startB, lenB, k-partA);
+        else if (A[startA+partA-1] > B[startB+partB-1]) // the partB is all range in the smaller k^th elements
+            return findKth(A, startA, lenA, B, startB+partB, lenB-partB, k-partB);
+        else
+            return A[startA+partA-1];
+    }
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size(), n=nums2.size(), total=m+n;
+        if (total % 2 == 1)
+            return findKth(nums1, 0, m, nums2, 0, n, total/2+1);
+        else
+            return (findKth(nums1, 0, m, nums2, 0, n, total/2) + findKth(nums1, 0, m, nums2, 0, n, total/2+1))/2;
+    }
+};
---
+++ #005.cpp
@@ -0,0 +1,33 @@
+/*============================================================
+Problem: Longest Palindromic Substring
+Given a string S, find the longest palindromic substring in S. 
+You may assume that the maximum length of S is 1000, and there 
+exists one unique longest palindromic substring.
+============================================================*/
+class Solution {
+private:
+    string expandAroundCenter(string s, int c1, int c2) {
+        int l = c1, r = c2;
+        int n = s.length();
+        while (l >= 0 && r <= n-1 && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        return s.substr(l+1, r-l-1);
+    }
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if (n==0) return "";
+        string res = s.substr(0, 1);  // a single char itself is a palindrome
+        for (int i=0; i<n-1; i++) {
+            string tmp = expandAroundCenter(s, i, i);
+            if (tmp.length() > res.length())
+                res = tmp;
+            tmp = expandAroundCenter(s, i, i+1);
+            if (tmp.length() > res.length())
+                res = tmp;
+        }
+        return res;
+    }
+};
