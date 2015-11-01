a3517ef732ce104bdde10ef35b2b5a87747a2676
Fan Li <fanli.cs.ucla@gmail.com>
2015-11-01 12:58:09 -0800

    code update

--- #005.cpp
+++ #005.cpp
@@ -6,29 +6,25 @@ You may assume that the maximum length of S is 1000, and there
 exists one unique longest palindromic substring.
 ============================================================*/
 class Solution {
-private:
-    string expandAroundCenter(string s, int c1, int c2) {
-        int l = c1, r = c2;
-        int n = s.length();
-        while (l >= 0 && r <= n-1 && s[l] == s[r]) {
-            l--;
-            r++;
-        }
-        return s.substr(l+1, r-l-1);
-    }
 public:
     string longestPalindrome(string s) {
-        int n = s.length();
-        if (n==0) return "";
-        string res = s.substr(0, 1);  // a single char itself is a palindrome
-        for (int i=0; i<n-1; i++) {
-            string tmp = expandAroundCenter(s, i, i);
-            if (tmp.length() > res.length())
-                res = tmp;
-            tmp = expandAroundCenter(s, i, i+1);
-            if (tmp.length() > res.length())
-                res = tmp;
+        if (s.size() < 2)
+            return s;
+        int len = s.size(), max_left = 0, max_len = 1, left, right;
+        for (int start = 0; start < len && len - start > max_len / 2;) {
+            left = right = start;
+            while (right < len - 1 && s[right + 1] == s[right])
+                ++right;
+            start = right + 1;
+            while (right < len - 1 && left > 0 && s[right + 1] == s[left - 1]) {
+                ++right;
+                --left;
+            }
+            if (max_len < right - left + 1) {
+                max_left = left;
+                max_len = right - left + 1;
+            }
         }
-        return res;
+        return s.substr(max_left, max_len);
     }
 };
